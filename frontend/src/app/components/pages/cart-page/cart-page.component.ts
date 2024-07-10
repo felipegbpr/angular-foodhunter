86812a5e2f9409e126df5b9ff528a76c7c2f7344
Felipe Borges <felipetecacc@gmail.com>
2024-07-10 16:17:12 -0300

    Connect To Backend

---
+++ frontend/src/app/components/pages/cart-page/cart-page.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { Cart } from 'src/app/shared/models/Cart';
+import { CartItem } from '../../../shared/models/CartItem';
+
+@Component({
+  selector: 'app-cart-page',
+  templateUrl: './cart-page.component.html',
+  styleUrls: ['./cart-page.component.css']
+})
+export class CartPageComponent implements OnInit {
+
+  cart!: Cart;
+
+  constructor(private cartService: CartService) {
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cart = cart;
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.food.id);
+  }
+
+  changeQuantity(cartItem: CartItem, quantityItem: string) {
+    const quantity = parseInt(quantityItem);
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+  }
+
+}
---
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Food } from 'src/app/shared/models/Food';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FoodService } from '../../../services/food.service';
+import { CartService } from '../../../services/cart.service';
+
+@Component({
+  selector: 'app-food-page',
+  templateUrl: './food-page.component.html',
+  styleUrls: ['./food-page.component.css']
+})
+export class FoodPageComponent implements OnInit {
+
+  food!: Food;
+
+  constructor(
+    activatedRoute: ActivatedRoute,
+    foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
+  ) {
+    activatedRoute.params.subscribe((params) => {
+      if (params.id)
+        foodService.getFoodById(params.id).subscribe(serverFood => {
+          this.food = serverFood;
+      });
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  addToCart() {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+  }
+}
